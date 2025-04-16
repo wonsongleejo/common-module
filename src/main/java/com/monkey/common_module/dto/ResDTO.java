ba7599fea5ee2ea98df972ca6dbbd36aa2ddb8cd
jiyun <jiyyy0720@naver.com>
2025-04-16 14:18:29 +0900

    feat: 공통모듈 설정 파일 추가 (공통예외처리, BaseEntity  등)

---
+++ src/main/java/com/monkey/common_module/dto/ResDTO.java
@@ -0,0 +1,35 @@
+package com.monkey.common_module.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class ResDTO<T> {
+    private String code;
+    private String message;
+    private T data;
+
+    // 성공 시 응답
+    public static <T> ResDTO<T> success(T data) {
+        return ResDTO.<T>builder()
+                .code(ResponseCode.SUCCESS.getCode())
+                .message(ResponseCode.SUCCESS.getMessage())
+                .data(data)
+                .build();
+    }
+
+    // 실패 시 응답
+    public static <T> ResDTO<T> fail(ResponseCode code) {
+        return ResDTO.<T>builder()
+                .code(code.getCode())
+                .message(code.getMessage())
+                .data(null)
+                .build();
+    }
+}
---
+++ src/main/java/com/monkey/common_module/exception/CustomException.java
@@ -0,0 +1,14 @@
+package com.monkey.common_module.exception;
+
+import com.monkey.common_module.dto.ResponseCode;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private final ResponseCode responseCode;
+
+    public CustomException(ResponseCode responseCode) {
+        super(responseCode.getMessage());
+        this.responseCode = responseCode;
+    }
+}
