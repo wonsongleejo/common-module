d3efa9705466743d52bfc5b01a11b3a763883ecc
cho <websera12@gmail.com>
2025-05-10 21:14:18 +0900

    redis host 변경

--- src/main/java/com/monkey/common_module/config/RedissonConfig.java
+++ src/main/java/com/monkey/common_module/config/RedissonConfig.java
@@ -13,7 +13,7 @@ public class RedissonConfig {
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress("redis://localhost:6379");
+                .setAddress("redis://52.78.194.135:6379");
         return Redisson.create(config);
     }
 }
